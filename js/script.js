26f7bf1cbd47d6ee5f19055a4d21ed5423346b3f
dmalataeva <deniza.malataeva@gmail.com>
2018-01-27 02:46:06 -0500

    Updated js scripts

---
+++ js/script.js
@@ -0,0 +1,59 @@
+$("#link-software").hover(
+	function() {
+		$("#rect-software").animate({
+			height: "1.2vw",
+			bottom: "1.2vw"
+		}, "fast");
+	}, 
+	function() {
+		$("#rect-software").animate({
+			height: "0.5vw",
+			bottom: "0.5vw"
+		}, "fast");
+	}
+);
+
+$("#link-art").hover(
+	function() {
+		$("#rect-art").animate({
+			height: "1vw",
+			bottom: "1vw"
+		}, "fast");
+	}, 
+	function() {
+		$("#rect-art").animate({
+			height: "0.5vw",
+			bottom: "0.5vw"
+		}, "fast");
+	}
+);
+
+$("#link-about").hover(
+	function() {
+		$("#rect-about").animate({
+			height: "1vw",
+			bottom: "1vw"
+		}, "fast");
+	}, 
+	function() {
+		$("#rect-about").animate({
+			height: "0.5vw",
+			bottom: "0.5vw"
+		}, "fast");
+	}
+);
+
+$("#link-resume").hover(
+	function() {
+		$("#rect-resume").animate({
+			height: "1.2vw",
+			bottom: "1.2vw"
+		}, "fast");
+	}, 
+	function() {
+		$("#rect-resume").animate({
+			height: "0.5vw",
+			bottom: "0.5vw"
+		}, "fast");
+	}
+);
